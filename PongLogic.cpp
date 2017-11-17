a3d8290a7271739cafd63fcead724a11676e6993
Thomas Frank <ga94ruq@mytum.de>
2017-11-17 18:53:04 +0100

    Started implementation of Logic

--- PongLogic.cpp
+++ PongLogic.cpp
@@ -3,13 +3,87 @@
 //
 
 #include "PongLogic.h"
+#include <stdlib>
+#include <time>
+#include <stdio>
 
-PongLogic::PongLogic(unsigned field_size, unsigned paddle_size) {
-    this->field_size=field_size;
-    this->paddle_size=paddle_size;
-    current_direction=UPRIGHT;
+/*
+*   PUBLIC
+*/
+
+PongLogic::PongLogic(unsigned field_size_x, unsigned field_size_y, unsigned paddle_size) {
+    std::srand(std::time(0));
+    this->field_size = field_size;
+    this->paddle_size = paddle_size;
+    current_direction = UPRIGHT;
+    paddle_pos_left = 0;
+    paddle_pos_right = 0;
+    ball_Pos_x = field_size_x/2;
+    ball_Pos_y = std::rand() % field_size_y;
+    switch (std::rand()%4) {
+      case 0:
+        current_direction=UPLEFT;
+        break;
+      case 1:
+        current_direction = UPRIGHT;
+        break;
+      case 2:
+        current_direction = DOWNRIGHT;
+        break;
+      case 3:
+        current_direction = DOWNLEFT;
+        break;
+      default:
+        std::cout << "Pong Logic Constructor Error\n";
+    }
 }
 
 PongLogic::~PongLogic() {
 
+}
+
+/*
+*   PRIVATE
+*/
+//TODO better way: use function pointer instead of enum
+void PongLogic::tick(){
+    switch (current_direction) {
+    case UPLEFT:
+      if (ball_Pos_x == 1) {
+        //test if paddle is in right place
+        if (paddle_left_is(ball_Pos_y)) {
+          if (ball_Pos_y == 0) {
+            ball_Pos_x++;
+            ball_Pos_y++;
+            current_direction = DOWNRIGHT;
+          }else{
+            ball_Pos_x++;
+            ball_Pos_y--;
+            current_direction = UPRIGHT;
+          }
+        }else{
+          //point for right side
+          points_right++;
+          reset;
+        }
+      }else{
+        if(ball_Pos_y==0){
+          ball_Pos_y++;
+          ball_Pos_x--;
+          current_direction = DOWNLEFT;
+        }else{
+          ball_Pos_x--;
+          ball_Pos_y--;
+        }
+      }
+      break;
+    case UPRIGHT:
+      break;
+    case DOWNLEFT:
+      break;
+    case DOWNRIGHT:
+      break;
+      default:
+       std::cout << "PongLogic Tick Error\n";
+    }
 }
--- PongLogic.h
+++ PongLogic.h
@@ -9,14 +9,26 @@
 
 class PongLogic {
 public:
-    PongLogic(unsigned field_size, unsigned paddle_size);
+    //initializes the game
+    PongLogic(unsigned field_size_x, unsigned field_size_y, unsigned paddle_size);
     ~PongLogic();
+
     enum direction{UPLEFT, UPRIGHT, DOWNRIGHT, DOWNLEFT};
+    void startgame();
 
 private:
     unsigned field_size, paddle_size;
-    unsigned x, y;
+    unsigned paddle_pos_left, paddle_pos_right;
+    unsigned ball_Pos_x, ball_Pos_y;
     direction current_direction;
+    unsigned points_left, points_right;
+
+    void tick();
+    void reset();
+
+    //test if paddle can catch ball
+    bool paddle_left_is(int y);
+    bool paddle_right_is(int y);
 };
 
 
